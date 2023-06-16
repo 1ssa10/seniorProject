fb4a69b0e4cf1243fdf45ba4a09af2ab32b76841
1ssa10 <mahmoudisaa10@gmail.com>
2023-06-17 00:19:32 +0300

    add marquee but need fix

--- src/components/Filmposter.js
+++ src/components/Filmposter.js
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
+import Marquee from "react-fast-marquee";
 
 function HandlePosterClick(id) {
   const url = `/Film/${id}`;
@@ -12,6 +13,7 @@ function HandlePosterClick(id) {
 
 function Filmposter() {
   const [cats, setCats] = useState([]);
+  const [isHovered, setIsHovered] = useState(false);
   useEffect(() => {
     async function fetchCats() {
       const response = await fetch("http://localhost:3000/api/filmsPosters");
@@ -26,30 +28,38 @@ function Filmposter() {
     <>
       {cats.map((cat) => (
         <div key={cat.id}>
-          <h2 className="mb-2 px-4 text-lg font-bold">{cat.catergory}</h2>
-          <div
-            className="film-container  flex overflow-x-auto  scrollbar-track-gray-950 scrollbar-thumb-slate-900 space-x-4"
-            id="style-2"
-          >
-            <div className="film-list ">
-              {cat.Films?.map((film) => (
-                <div key={film.id} className=" flex-none w-30">
-                  <div className="film-item flex flex-col items-center mt-2">
-                    <Image
-                      src={film.image}
-                      alt="Film Poster"
-                      width={180}
-                      height={320}
-                      onClick={() => {
-                        HandlePosterClick(film.id);
-                      }}
-                    />
+          <h2 className="mb-2 px-4 text-lg font-bold text-center text-red-900">
+            {cat.catergory}
+          </h2>
+          <Marquee play={isHovered ? true : false} autoFill>
+            <div
+              className="film-container  flex overflow-x  scrollbar-track-gray-950 scrollbar-thumb-slate-900 space-x-4"
+              id="style-2"
+            >
+              <div
+                className="film-list "
+                onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={() => setIsHovered(false)}
+              >
+                {cat.Films?.map((film) => (
+                  <div key={film.id} className=" flex-none w-30">
+                    <div className="film-item flex flex-col items-center mt-2">
+                      <Image
+                        src={film.image}
+                        alt="Film Poster"
+                        width={180}
+                        height={320}
+                        onClick={() => {
+                          HandlePosterClick(film.id);
+                        }}
+                      />
+                    </div>
+                    <p>{film.title}</p>
                   </div>
-                  <p>{film.title}</p>
-                </div>
-              ))}
+                ))}
+              </div>
             </div>
-          </div>
+          </Marquee>
         </div>
       ))}
     </>
