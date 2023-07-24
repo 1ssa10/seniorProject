726f1e0f9b7ef17f6b8c95494f5a4d5d8aefc8e6
1ssa10 <mahmoudisaa10@gmail.com>
2023-07-24 14:11:17 +0300

    swiper actors

--- src/app/Film/[id]/page.js
+++ src/app/Film/[id]/page.js
@@ -1,19 +1,15 @@
 "use client";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
-import Loading from "./loading";
+
 import { useEffect, useState } from "react";
 import { Carousel, Rating, Typography } from "@material-tailwind/react";
 import ProfileImage from "@/components/ProfileImage";
 import ReactSpeedometer from "react-d3-speedometer";
-import { DateTime } from "luxon";
-import Averagerate from "@/components/Averagerate";
-import {
-  ButtonBack,
-  ButtonNext,
-  CarouselProvider,
-  Slider,
-} from "pure-react-carousel";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination, Navigation } from "swiper/modules";
 
 function Page({ params }) {
   // const url = window.location.href;
@@ -148,86 +144,36 @@ function Page({ params }) {
           <img src={film?.image} alt="film poster" width={280} height={420} />
         </div>
         <div className="flex flex-col justify-end">
-          <CarouselProvider
-            className="lg:block hidden"
-            naturalSlideWidth={100}
-            isIntrinsicHeight={true}
-            totalSlides={12}
-            visibleSlides={4}
-            step={1}
-            infinite={true}
-          >
-            <div className="w-full relative flex items-center justify-center">
-              <ButtonBack
-                role="button"
-                aria-label="slide backward"
-                className="absolute z-30 left-0 ml-8 focus:outline-none focus:bg-gray-400 focus:ring-2 focus:ring-offset-2 focus:ring-gray-400 cursor-pointer"
-                id="prev"
-              >
-                <svg
-                  width={8}
-                  height={14}
-                  viewBox="0 0 8 14"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M7 1L1 7L7 13"
-                    stroke="white"
-                    strokeWidth={2}
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                </svg>
-              </ButtonBack>
-              <div className="w-full h-full mx-auto overflow-x-hidden overflow-y-hidden">
-                <Slider>
-                  <div
-                    id="slider"
-                    className="h-full flex lg:gap-8 md:gap-6 gap-14 items-center justify-start transition ease-out duration-700"
-                  >
-                    {film?.Actors?.map((actor) => (
-                      <div key={actor.id} className="flex items-center mb-4">
-                        <div>
-                          <Image
-                            src={actor?.image}
-                            alt="Actor Image"
-                            width={100}
-                            height={120}
-                          />
-                          <p className="ml-4">
-                            {actor.first_name} <br /> {actor.last_name}
-                          </p>
-                        </div>
-                      </div>
-                    ))}
-                  </div>
-                </Slider>
-              </div>
-              <ButtonNext
-                role="button"
-                aria-label="slide forward"
-                className="absolute z-30 right-0 mr-8 focus:outline-none focus:bg-gray-400 focus:ring-2 focus:ring-offset-2 focus:ring-gray-400"
-                id="next"
-              >
-                <svg
-                  width={8}
-                  height={14}
-                  viewBox="0 0 8 14"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M1 1L7 7L1 13"
-                    stroke="white"
-                    strokeWidth={2}
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                </svg>
-              </ButtonNext>
-            </div>
-          </CarouselProvider>
+          <div className="w-fit h-fit mx-auto overflow-x-hidden overflow-y-hidden bg-blue-gray-900">
+            <Swiper
+              slidesPerView={4}
+              spaceBetween={10}
+              pagination={{
+                clickable: true,
+              }}
+              navigation={true}
+              modules={[Pagination, Navigation]}
+              className=" max-w-3xl"
+            >
+              {film?.Actors?.map((actor, index) => (
+                <div key={actor.id} className="flex items-center mb-4">
+                  <SwiperSlide>
+                    <div>
+                      <Image
+                        src={actor?.image}
+                        alt="Actor Image"
+                        width={100}
+                        height={120}
+                      />
+                      <p className="ml-4">
+                        {actor.first_name} <br /> {actor.last_name}
+                      </p>
+                    </div>
+                  </SwiperSlide>
+                </div>
+              ))}
+            </Swiper>
+          </div>
         </div>
       </div>
 
